41bb81da9a64a1fad7f365e50554b87b133f363c
Isasouza00 <isasouza20190701@gmail.com>
2021-12-02 22:29:33 -0300

    okay

---
+++ cinemonk/src/pages/conclusion/index.js
@@ -0,0 +1,18 @@
+
+
+export default function conclusion() {
+    return (
+        <Conteiner_conclusion>
+
+            <div className="f1"> 
+                <div className="logo"><img src="/assets/images/ticket-logo" alt="CineMonk"/> CineMonk </div>
+                <div className="message"> Parabéns! Sua compra foi realizada com sucesso.</div>
+                <div className="message"> Agora é só aproveitar o filme :) </div> 
+            </div>
+
+
+            <div className="menu_button"><button> Menu </button></div>
+
+        </Conteiner_conclusion>
+    )
+}
---
+++ cinemonk/src/routes.js
@@ -0,0 +1,13 @@
+import { BrowserRounter, Switch, Route } from "react-route-dom";
+import Conclusion from '.pages/conclusion';
+
+
+export default function Routes() {
+    return (
+        <BrowserRounter>
+            <Switch>
+                <Route path="/conclusion" exact={true} component={Conclusion} />
+            </Switch>
+        </BrowserRounter>
+    )
+}
