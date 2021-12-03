51b6eacea631a45caa42700f54992cb208ef1386
LucianeC <costaluciane087@gmail.com>
2021-12-02 23:52:16 -0300

    ok

--- cinemonk/src/routes.js
+++ cinemonk/src/routes.js
@@ -1,5 +1,6 @@
 import { BrowserRounter, Switch, Route } from "react-router-dom";
 import Conclusion from './pages/conclusion';
+import Seat from '.pages/selection-seat';
 
 
 export default function Routes() {
@@ -7,6 +8,7 @@ export default function Routes() {
         <BrowserRounter>
             <Switch>
                 <Route path="/conclusion" exact={true} component={Conclusion} />
+                <Route path="/selction-seat" exact={true} component= {Seat} />
             </Switch>
         </BrowserRounter>
     )
