7e00bf3dda19a1c22ae8384ea040fd613087f1fa
Davi <davii.clink@gmail.com>
2021-12-04 00:04:23 -0300

    routes corrigida

--- cinemonk/src/components/CineMonk-logo/styled.js
+++ cinemonk/src/components/CineMonk-logo/styled.js
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
 const CineMonk = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 
 .title{
-    color: rgba(240, 161, 36, 1);  
+    color: rgba(240, 161, 36, 1);
+    font: 64px Seoul-Hangang;
+    font-weight: 400;
+}
+
+.ticket{
+    width: 70px;
+    margin-right: 20px;
 }
 `
 export{CineMonk}
--- cinemonk/src/pages/selection-hours/index.js
+++ cinemonk/src/pages/selection-hours/index.js
@@ -6,6 +6,7 @@ export default function SelectionHours() {
         <Container>
             <div className="cabecalho">
                 <Cinemonk />
+                <div> ola </div>
             </div>
         </Container>
     )
