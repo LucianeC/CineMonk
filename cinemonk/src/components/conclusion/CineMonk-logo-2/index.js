8f38571ce50f65a7d85e665537b546f378fcad3f
Isasouza00 <isasouza20190701@gmail.com>
2021-12-05 18:45:27 -0300

    socorro

---
+++ cinemonk/src/components/conclusion/CineMonk-logo-2/index.js
@@ -0,0 +1,11 @@
+import {Container_logo} from './styled'
+
+export default function CineMonk(){
+    return (
+        <Container_logo>
+            <div className="logo"><img src= "assets/images/ticket-logo.png" alt=""/></div>
+            <div className="title"> CineMonk </div>
+        </Container_logo>
+    )
+}
+
--- cinemonk/src/pages/conclusion/index.js
+++ cinemonk/src/pages/conclusion/index.js
@@ -1,21 +1,37 @@
-import { Containerconclusion } from "./styled";
-import Logo from "../../components/CineMonk-logo-2/index";
-import Ticket from "../../components/ticket_sucess/index";
+import { Container_conclusion } from "./styled";
+import Logo from "../../components/conclusion/CineMonk-logo-2/index";
+import Ticket from "../../components/conclusion/ticket_sucess/index";
 
 export default function Conclusion() {
     return (
-        <Containerconclusion>
+        <Container_conclusion>
+
+
             <Logo/>
+
 
             <div className="f1-message"> 
                 <div className="message"> Parabéns! Sua compra foi realizada com sucesso.</div>
                 <div className="message"> Agora é só aproveitar o filme :) </div> 
             </div>
 
-            <Ticket/>
+            
+            <div className="Tickets"> 
+                <Ticket/>
+
+                <Ticket/>
+
+                <Ticket/>
+
+                <Ticket/>
+
+                <Ticket/>
+            </div>
+
 
             <div className="menu_button"><button> Menu </button></div>
 
-        </Containerconclusion>
+
+        </Container_conclusion>
     )
 }
